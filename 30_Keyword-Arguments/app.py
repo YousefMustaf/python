3358f5836258950210376dff5c82022cd4f67356
Dark <133033069+YousefMustaf@users.noreply.github.com>
2026-09-10 22:56:50 +0000

    FilesUpdate

--- 30_Keyword-Arguments/app.py
+++ 30_Keyword-Arguments/app.py
@@ -8,5 +8,4 @@ greet_user("Youssef", "Mostafa") # this is a positional argummensts and from the
 greet_user(last_name="Mostafa", first_name="Youssef") # this is why in the keyword argument we do not worry about the position or the argument.
 # and its more practical to use keyword_argument instead of position argummetn.
 
-calc_cost(total=50, shipping=5, discount=0.1) # this is when the keywords arguments gets very useful
 
