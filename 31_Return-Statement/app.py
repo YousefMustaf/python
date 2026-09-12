be23098060e1303cc16cb462354486cbd93d32b0
Dark <133033069+YousefMustaf@users.noreply.github.com>
2026-09-12 00:19:00 +0000

    FilesUpdate

---
+++ 31_Return-Statement/app.py
@@ -0,0 +1,11 @@
+# def greet_user(first_name, last_name):
+#     print(f'Hi {first_name} {last_name}!')
+#     print("Welcome to the Society")
+
+def square(number):
+    return number * number
+
+
+
+print(square(int(input("Enter a nummber\n"))))
+# this takes the number that the use inputs and apply the function on it.
---
+++ 32_Creating-a-Reusable-Function/app.py
@@ -0,0 +1,14 @@
+def emoji_converter(message):
+    words = message.split(" ")
+    emojis = {
+        ":)" : "😊",
+        ":(" : "😔",
+        "<3" : "❤️"
+    }
+    output = ""
+    for word in words:
+        output += emojis.get(word, word) + " "
+    return output
+
+message = input("> ")
+print(emoji_converter(message))
